806f9bcbea6e63204b324cf2c84ca22ff0b9a788
Thompson B. Crozier <cybertrebuchet@gmail.com>
2015-08-13 11:23:01 -0400

    seeded data/topics.db

--- data/schema.sql
+++ data/schema.sql
@@ -1,10 +1,16 @@
 CREATE TABLE topics (
   id integer PRIMARY KEY AUTOINCREMENT,
+  approval integer,
   title text,
   author text,
-  timestamp
+  comments integer
 );
 
 CREATE TABLE comments (
-
+  id integer PRIMARY KEY AUTOINCREMENT,
+  approval integer,
+  author text,
+  content text,
+  comments integer,
+  parent_id varchar -- formatted t0 or t0c0
 );
---
+++ data/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO topics (approval, title, author, comments) VALUES (0, 'teh best topic evar', 'thompson!', 3);
+INSERT INTO comments (approval, author, content, comments, parent_id) VALUES (0, 'thompson!', 'wow I could talk about this topic forevar', 2, 't0');
+INSERT INTO comments (approval, author, content, comments, parent_id) VALUES (0, 'thompson!', 'lk srsly wt a grt tpc', 1, 't0c0');
+INSERT INTO comments (approval, author, content, comments, parent_id) VALUES (0, 'thompson!', 'lk srsly wt a grt tpc', 0, 't0c1');
