3be38c88e5d31701e80d6c947939f8d05c202aca
Thompson B. Crozier <cybertrebuchet@gmail.com>
2015-08-13 16:27:08 -0400

    working on topic page with comments

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ var _       = require("underscore");
 var sqlite3 = require("sqlite3").verbose();
 var db      = new sqlite3.Database("data/forOM.db");
 
-console.log(db);
-
 //publicize ./views for css and js
 app.use(express.static('public'));
 
@@ -29,11 +27,9 @@ app.listen(3000, function() {
 
 //routes
 app.get("/", function(req, res) {
-  console.log("/ route triggered");
   db.all("SELECT * FROM topics", function(err, rows){
     if (err) {console.log(err)} else {
       var topics = rows;
-      console.log(topics);
       var html = fs.readFileSync("views/index.html", "utf8");
       var rendered = ejs.render(html, {topics: topics});
       res.send(rendered);
@@ -42,11 +38,13 @@ app.get("/", function(req, res) {
 });
 
 app.post("/topics", function(req, res) {
-  console.log("/topics route triggered");
-  console.log(req.body);
   db.run("INSERT INTO topics (approval, title, author, comments) VALUES (?,?,?,?)", 0, req.body.topic, req.body.author, 0, function(err){
     if (err) {console.log(err)} else {res.redirect("/")}
   });
+});
+
+app.get("/topics/:id", function(req, res) {
+  console.log(req.params.id);
 });
 
 app.get("/search", function(req, res) {
