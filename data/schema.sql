39de8ffcc86b3610531827054080c4e2d2d5f952
Thompson B. Crozier <cybertrebuchet@gmail.com>
2015-08-14 14:18:55 -0400

    blinks

--- data/schema.sql
+++ data/schema.sql
@@ -1,18 +1,8 @@
-CREATE TABLE topics (
+CREATE TABLE blinks (
   id integer PRIMARY KEY AUTOINCREMENT,
-  topic_id varchar, -- formatted t1
+  parent_id integer,
   approval integer,
   title text,
   author text,
   comments integer
-);
-
-CREATE TABLE comments (
-  id integer PRIMARY KEY AUTOINCREMENT,
-  comment_id varchar, -- formatted t1c1
-  approval integer,
-  author text,
-  content text,
-  comments integer,
-  parent_id varchar -- formatted t1 or t1c1
 );
